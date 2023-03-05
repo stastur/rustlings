cf37b512b5b3a92a1e275f968c7c5d565618e588
Stanislau Tur <stanislau.tur@gmail.com>
2023-03-05 13:34:52 +0100

    Take 2 - functions - if - primitive types - vectors

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,19 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apples: u32) -> u32 {
+    const default_price: u32 = 2;
+    const discount_price: u32 = 1;
+    const quantity_discount: u32 = 40;
+
+    apples
+        * if apples <= quantity_discount {
+            default_price
+        } else {
+            discount_price
+        }
+}
 
 // Don't modify this function!
 #[test]
